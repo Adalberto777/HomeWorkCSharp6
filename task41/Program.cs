ebd0631c556f7b43aead5f4f9a9991e2b90186ab
Adalberto777 <albert.tliashev@gmail.com>
2022-09-10 15:29:44 +0500

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+Console.WriteLine("введите числа через пробел");
+string[] st = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries); //считывает строку, когда видит пробел отделяет выражение и записывает в массив строк
+                                                                              
+int CounterPozitiveNumber(string[] st1) 
+{ 
+    int count = 0;
+    foreach (string var in st1)
+    {
+        if(int.Parse(var) > 0)
+        {
+            count += 1;
+        }
+    }
+    return count; 
+}
+
+Console.WriteLine($"чисел > 0: {CounterPozitiveNumber(st)}");
+
+
+//int.Parse(st[0])
